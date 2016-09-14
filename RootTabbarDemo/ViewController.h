0fdb17388fd789f8551e76a2c8e2e3edc44c3f1f
guang <331689079@qq.com>
2016-09-14 08:51:42 +0800

    Initial Commit

---
+++ RootTabbarDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RootTabbarDemo
+//
+//  Created by 光 on 16/9/14.
+//  Copyright © 2016年 光. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RootTabbarDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  RootTabbarDemo
+//
+//  Created by 光 on 16/9/14.
+//  Copyright © 2016年 光. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
